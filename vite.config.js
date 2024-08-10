e64e4c6ceb7e8b16b2da26943749c93a77ca76eb
RakaFadilDwiA <rakafadildwi@gmail.com>
2024-08-10 15:09:49 +0700

    Test Deploy Vercel

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [
+      vue(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  })
+}
+
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: process.env.VITE_API_URL,
+//         changeOrigin: true,
+//       },
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
